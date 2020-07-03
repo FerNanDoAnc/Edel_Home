2f4af7c3008a159ed30ff8ef1495d50d697298b6
benjamin <benja.villajuan@gmail.com>
2020-07-03 16:20:29 -0500

    on/off bulb funcionallity

--- app/src/main/res/drawable/proyecto_arduino.ino
+++ app/src/main/res/drawable/proyecto_arduino.ino
@@ -1,32 +1,37 @@
-int led2=5;
-int led3=6;
-int led4=7;
-char foco;
+int led = 2;
+int led1 = 3;
+int led2 = 4;
 char dato;
 
 void setup() {
-  pinMode(led2,OUTPUT);
-  pinMode(led3,OUTPUT);
-  pinMode(led4,OUTPUT);
+  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  if(Serial.available()>0){
-    dato=Serial.read();
-  if(dato=='1'){
-         if (dato=='1'){  
-           digitalWrite(led2,HIGH);}
-         if(dato=='0'){
-            digitalWrite(led2,LOW);}
-              }
-  if(dato=='2'){
-        if (dato=='1'){  
-           digitalWrite(led2,HIGH);}
-        if(dato=='0'){
-            digitalWrite(led2,LOW);}
-              }
-  
+  if (Serial.available() > 0) {
+    dato = Serial.read();
+
+  if(dato == '1'){
+      digitalWrite(led, HIGH);
+  }
+  if (dato == '0') {
+      digitalWrite(led, LOW);
+  }
+    if (dato == '3') {
+      digitalWrite(led1, HIGH);
+  }
+    if (dato == '2') {
+      digitalWrite(led1, LOW);
+  }
+    if (dato == '5') {
+      digitalWrite(led2, HIGH);
+  }
+    if (dato == '4') {
+      digitalWrite(led2, LOW);
   }
 
+}
 }
