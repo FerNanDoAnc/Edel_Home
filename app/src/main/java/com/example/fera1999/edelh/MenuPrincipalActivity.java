d9c7bd057a01b4a9c2c687ce7813068a3847f1c6
Fernando A <fernando.ancajima99@gmail.com>
2020-07-15 12:55:34 -0500

    AVANCE MENU

--- app/src/main/java/com/example/fera1999/edelh/MenuPrincipalActivity.java
+++ app/src/main/java/com/example/fera1999/edelh/MenuPrincipalActivity.java
@@ -1,18 +1,36 @@
 package com.example.fera1999.edelh;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
 
     TextView user;
 
+    DrawerLayout drawerlayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerlayout= findViewById(R.id.drawer);
+        navigationView=findViewById(R.id.navigationView);
+
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerlayout, toolbar,R.string.open,R.string.close);
+        drawerlayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
 
 
         user=findViewById(R.id.usuario);
