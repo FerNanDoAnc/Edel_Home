8e79aa5ca98da954bf82ea9f4bdaf59cb483bb78
A17204446@idat.edu.pe <A17204446@idat.edu.pe>
2020-07-17 11:23:41 -0500

    Se agrego el switch para cambiar el color de fondo.

--- app/src/main/java/com/example/fera1999/edelh/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/fera1999/edelh/ui/home/HomeFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -17,13 +21,28 @@ import com.example.fera1999.edelh.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    View root;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+         root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
+
+        final Switch encendidoapagdo=root.findViewById(R.id.encendidoapagdo);
+        encendidoapagdo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (encendidoapagdo.isChecked()){
+                  root.setBackgroundResource(R.drawable.azul);
+                }else{
+                  root.setBackgroundResource(R.drawable.negro);
+
+                }
+            }
+        });
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
