1686295b908eb20863917a8cd212038a23f75882
A17204446@idat.edu.pe <A17204446@idat.edu.pe>
2020-07-20 12:26:40 -0500

    Listado de usuarios desde mysql.

--- app/src/main/java/com/example/fera1999/edelh/ui/users/UsersFragment.java
+++ app/src/main/java/com/example/fera1999/edelh/ui/users/UsersFragment.java
@@ -1,10 +1,14 @@
 package com.example.fera1999.edelh.ui.users;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,23 +17,109 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.fera1999.edelh.R;
+import com.example.fera1999.edelh.clases.Usuario;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class UsersFragment extends Fragment {
 
     private UsersViewModel galleryViewModel;
+    ListView listView;
+
+    private String idgrupousuario="1";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(UsersViewModel.class);
         View root = inflater.inflate(R.layout.fragment_users, container, false);
+
+        /*
         final TextView textView = root.findViewById(R.id.text_gallery);
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        }); */
+
+            listView = (ListView) root.findViewById(R.id.listausuario);
+            downloadJSON("http://192.168.43.246/getGroupUsers.php");
+
         return root;
+
     }
+
+    private void downloadJSON(final String urlWebService) {
+
+        class DownloadJSON extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                    loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL url = new URL(urlWebService);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        DownloadJSON getJSON = new DownloadJSON();
+        getJSON.execute();
+    }
+
+    private void loadIntoListView(String json) throws JSONException {
+        JSONArray jsonArray = new JSONArray(json);
+        String[] stocks = new String[jsonArray.length()];
+            ArrayList<Usuario> usuariolista = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject obj = jsonArray.getJSONObject(i);
+            Usuario ul=new Usuario();
+            ul.setNombre(obj.getString("username"));
+            ul.setLastlogin("last_login");
+            stocks[i] = obj.getString("username") + " " + obj.getString("last_login");
+            System.out.println(stocks[i]);
+            usuariolista.add(ul);
+            System.out.println("LISTA "+usuariolista);
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_multiple_choice,stocks);
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), R.layout.itemusuario,stocks);
+
+        listView.setAdapter(arrayAdapter);
+
+    }
+
+
 }
