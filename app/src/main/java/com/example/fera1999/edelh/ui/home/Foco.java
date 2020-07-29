07455043588bfe71a2f7e7c92bbb975d684e0d57
Fera1999 <fernando.ancajima99@gmail.com>
2020-07-29 12:21:06 -0500

    cambio de color del spinner

--- app/src/main/java/com/example/fera1999/edelh/ui/home/Foco.java
+++ app/src/main/java/com/example/fera1999/edelh/ui/home/Foco.java
@@ -3,14 +3,17 @@ package com.example.fera1999.edelh.ui.home;
 public class Foco {
     private int switch_id;
     private String place;
-
+    private int bulb_state;
+    private int group_id;
 
     public Foco() {
     }
 
-    public Foco(int switch_id, String place) {
+    public Foco(int switch_id, String place, int bulb_state, int group_id) {
         this.switch_id = switch_id;
         this.place = place;
+        this.bulb_state = bulb_state;
+        this.group_id = group_id;
     }
 
     public int getSwitch_id() {
@@ -27,6 +30,22 @@ public class Foco {
 
     public void setPlace(String place) {
         this.place = place;
+    }
+
+    public int getBulb_state() {
+        return bulb_state;
+    }
+
+    public void setBulb_state(int bulb_state) {
+        this.bulb_state = bulb_state;
+    }
+
+    public int getGroup_id() {
+        return group_id;
+    }
+
+    public void setGroup_id(int group_id) {
+        this.group_id = group_id;
     }
 
     @Override
