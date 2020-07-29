3818cc357999c6343d2834a1e96ee549a457df18
Fera1999 <fernando.ancajima99@gmail.com>
2020-07-29 12:31:12 -0500

    list switch==id_group

--- app/src/main/res/assets/edelhome/listSwitch.php
+++ app/src/main/res/assets/edelhome/listSwitch.php
@@ -1,14 +1,25 @@
 <?php
 
 include 'connection.php';
-# declaramos las variables a usar, $_POST significa los datos que hemos enviado a este archivo
-$sql ="select * from switch";
-$datos=Array();
-$result=mysqli_query($connection,$sql);
-while($row=mysqli_fetch_object($result)){
-	$datos[]=$row;
+$group_id=$_GET['group_id'];
+
+$consulta ="select * from switch where group_id='$group_id'";
+$resultado=$connection -> query($consulta);
+
+while($fila=$resultado -> fetch_array()){
+    $switch[] = array_map('utf8_encode',$fila);
 }
-echo json_encode($datos);
-# Cerramos la conexion
-mysqli_close($connection);
+
+echo json_encode($switch);
+$resultado -> close();
+
+#include 'connection.php';
+#$sql ="select * from switch";
+#$datos=Array();
+#$result=mysqli_query($connection,$sql);
+#while($row=mysqli_fetch_object($result)){
+#	$datos[]=$row;
+#}
+#echo json_encode($datos);
+#mysqli_close($connection);
 ?>
