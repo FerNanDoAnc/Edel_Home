a3c2b066b8b703142635c8b8548f2568f401a857
Benjamin Villajuan <47691286+theonlybenjamin@users.noreply.github.com>
2020-08-08 14:31:01 -0500

    Update listSwitch.php

--- app/src/main/res/assets/edelhome/listSwitch.php
+++ app/src/main/res/assets/edelhome/listSwitch.php
@@ -1,15 +1,15 @@
 <?php
 
 include 'connection.php';
-$group_id=$_GET['group_id'];
-
-$consulta ="call getGroupSwitches('".$group_id."')";
-$resultado=$connection -> query($consulta);
-
-while($fila=$resultado -> fetch_array()){
-    $switch[] = array_map('utf8_encode',$fila);
+$var_group_id = $_GET['group_id'];
+# declaramos las variables a usar, $_POST significa los datos que hemos enviado a este archivo
+$sql ="call getGroupSwitches('".$var_group_id."')";
+$datos=Array();
+$result=mysqli_query($connection,$sql);
+while($row=mysqli_fetch_object($result)){
+	$datos[]=$row;
 }
-
-echo json_encode($switch);
-$resultado -> close();
+echo json_encode($datos);
+# Cerramos la conexion
+mysqli_close($connection);
 ?>
